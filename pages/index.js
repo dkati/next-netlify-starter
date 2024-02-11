b3597b12b93ae2f096df851e485d558b4648301f
Dimitris Katikaridis <dkatikaridis@gmail.com>
2024-02-11 21:52:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,31 +16,36 @@ export default function InputPage() {
      const months0 = (document.getElementById('months-0').value === "" ? 0 : document.getElementById('months-0').value);
      const permonth0 = amount0Value / (months0 === 0 ? 1 : months0);
      const fpa0 = (document.getElementById('vat-0').checked ? (permonth0 - (permonth0 / 1.24)) : 0 );
-     const foros0 = (document.getElementById('withholding-0').checked ? (permonth0 - (permonth0 / 1.20)) : 0 );
+     const permonth0Clear = permonth0-fpa0;
+     const foros0 = (document.getElementById('withholding-0').checked ? (permonth0Clear - (permonth0Clear / 1.20)) : 0 );
 
      const amount1Value = (document.getElementById('amount-1').value === "" ? 0 : document.getElementById('amount-1').value);
      const months1 = (document.getElementById('months-1').value === "" ? 0 : document.getElementById('months-1').value);
      const permonth1 = amount1Value / (months1 === 0 ? 1 : months1);
      const fpa1 = (document.getElementById('vat-1').checked ? (permonth1 - (permonth1 / 1.24))  : 0 );
-     const foros1 = (document.getElementById('withholding-1').checked ? (permonth1 - (permonth1 / 1.20))  : 0 );
+     const permonth1Clear = permonth1-fpa1;
+     const foros1 = (document.getElementById('withholding-1').checked ? (permonth1Clear - (permonth1Clear / 1.20)) : 0 );
    
      const amount2Value = (document.getElementById('amount-2').value === "" ? 0 : document.getElementById('amount-2').value);
      const months2 = (document.getElementById('months-2').value === "" ? 0 : document.getElementById('months-2').value);
      const permonth2 = amount2Value / (months2 === 0 ? 1 : months2);
-     const fpa2 = (document.getElementById('vat-2').checked ? (permonth2 - (permonth2 / 1.24))  : 0 );
-     const foros2 = (document.getElementById('withholding-2').checked ? (permonth2 - (permonth2 / 1.20)) : 0 );
+     const fpa2 = (document.getElementById('vat-2').checked ? (permonth2 - (permonth2 / 1.24)) : 0 );
+     const permonth2Clear = permonth2-fpa2;
+     const foros2 = (document.getElementById('withholding-2').checked ? (permonth2Clear - (permonth2Clear / 1.20)) : 0 );
    
      const amount3Value = (document.getElementById('amount-3').value === "" ? 0 : document.getElementById('amount-3').value);
      const months3 = (document.getElementById('months-3').value === "" ? 0 : document.getElementById('months-3').value);
      const permonth3 = amount3Value / (months3 === 0 ? 1 : months3);
      const fpa3 = (document.getElementById('vat-3').checked ? (permonth3 - (permonth3 / 1.24)) : 0 );
-     const foros3 = (document.getElementById('withholding-3').checked ? (permonth3 - (permonth3 / 1.20)) : 0 );
+     const permonth3Clear = permonth3-fpa3;
+     const foros3 = (document.getElementById('withholding-3').checked ? (permonth3Clear - (permonth3Clear / 1.20)) : 0 );
    
      const amount4Value = (document.getElementById('amount-4').value === "" ? 0 : document.getElementById('amount-4').value);
      const months4 = (document.getElementById('months-4').value === "" ? 0 : document.getElementById('months-4').value);
      const permonth4 = amount4Value / (months4 === 0 ? 1 : months4);
      const fpa4 = (document.getElementById('vat-0').checked ? (permonth4 - (permonth4 / 1.24)) : 0 );
-     const foros4 = (document.getElementById('withholding-4').checked ?  (permonth4 - (permonth4 / 1.20)) : 0 );
+     const permonth4Clear = permonth4-fpa4;
+     const foros4 = (document.getElementById('withholding-1').checked ? (permonth4Clear - (permonth4Clear / 1.20)) : 0 );
 
      const withFpa = permonth0 + permonth1 + permonth2 + permonth3 + permonth4;
 

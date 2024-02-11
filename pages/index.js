1ff46a644593f89b240834152784dccee3614edd
Dimitris Katikaridis <dkatikaridis@gmail.com>
2024-02-11 20:35:34 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,23 @@ export default function InputPage() {
     // Here you can handle the submission logic, e.g., calculating amounts, validating inputs, etc.
      const amount0Value = document.getElementById('amount-0').value;
      const months0 = document.getElementById('months-0').value;
-     const permonth0 = amount1Value / months;
+     const permonth0 = amount0Value / months0;
 
      const amount1Value = document.getElementById('amount-1').value;
      const months1 = document.getElementById('months-1').value;
-     const permonth1 = amount1Value / months;
+     const permonth1 = amount1Value / months1;
 
      const amount2Value = document.getElementById('amount-2').value;
      const months2 = document.getElementById('months-2').value;
-     const permonth2 = amount1Value / months;
+     const permonth2 = amount2Value / months2;
 
      const amount3Value = document.getElementById('amount-3').value;
      const months3 = document.getElementById('months-3').value;
-     const permonth3 = amount1Value / months;
+     const permonth3 = amount3Value / months3;
 
      const amount4Value = document.getElementById('amount-4').value;
      const months4 = document.getElementById('months-4').value;
-     const permonth4 = amount1Value / months;
+     const permonth4 = amount4Value / months4;
 
      const permonth = permonth0 + permonth1 + permonth2 + permonth3 + permonth4
      console.log('per month:', permonth);
